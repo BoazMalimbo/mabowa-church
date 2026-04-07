a671a616524af3783fb2cbea658d06b2159f7c1e
BoazMalimbo <malimboboaz@gmail.com>
2026-04-07 23:39:24 +0200

    la classe qui a le model pour les donnees des depenses

---
+++ depenseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eglise_mambowa
+{
+    internal class depenseModel
+    {
+        public DateTime Date { get; set; }
+        public string TypeDepense { get; set; }
+        public decimal Montant { get; set; }
+        public string Description { get; set; }
+        public int IdUser { get; set; }
+    }
+}
