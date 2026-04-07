36b6b642a81b6903d98c3e294be4eb3da68d4c48
BoazMalimbo <malimboboaz@gmail.com>
2026-04-07 12:53:07 +0200

    premier commit

---
+++ connexionDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eglise_mambowa
+{
+    internal class connexionDB
+    {
+        private string chaine = @"Data Source=MALIMBO\SQLEXPRESS;Initial Catalog=gestionRevenus;Integrated Security=True";
+        public SqlConnection GetConnexion()
+        {
+            SqlConnection con = new SqlConnection(chaine);
+            return con;
+        }
+    }
+}
