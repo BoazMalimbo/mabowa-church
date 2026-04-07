346dcaf6a0562bc15100bc68b14fc88ef405dc6a
BoazMalimbo <malimboboaz@gmail.com>
2026-04-07 16:55:40 +0200

    la classe pour le modele des revenus

---
+++ revenuModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eglise_mambowa
+{
+    internal class revenuModel
+    {
+        // Propriétés avec getters et setters
+        public DateTime Date { get; set; }
+        public string Typerevenu { get; set; }
+        public decimal Montant { get; set; }
+        public string Description { get; set; }
+        public int IdUser { get; set; }
+    }
+}
