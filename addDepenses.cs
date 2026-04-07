a2d3c01de3ea29acff10307569af3100c5621fd9
BoazMalimbo <malimboboaz@gmail.com>
2026-04-07 23:40:09 +0200

    ajouter des depenses dans la base des donnees

--- addDepenses.cs
+++ addDepenses.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,35 @@ namespace eglise_mambowa
         public addDepenses()
         {
             InitializeComponent();
+        }
+
+        connexionDB db = new connexionDB();
+
+        private void tbn_enregistrer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                revenuModel rvml = new revenuModel();
+
+                rvml.Date = dateTimePicker1.MaxDate;
+                rvml.Typerevenu = tb_categorie.Text;
+                rvml.Montant = decimal.Parse(tb_montant.Text);
+                rvml.Description = rtb_description.Text;
+                rvml.IdUser = 1;
+
+                // on instancie la classe revenu pour avoir acces a la mathode ajouterRevenu
+                revenu revenu = new revenu();
+                revenu.ajouterRevenu(rvml);
+
+                tb_montant.Clear();
+                rtb_description.Clear();
+
+                MessageBox.Show("l'enregistrement a réussi avec succès");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur" + ex);
+            }
         }
     }
 }
